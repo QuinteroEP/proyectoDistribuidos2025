3e2e7ff3f7a8566c84eec90885c7e8c554df3292
Pablo Enrique Quintero Callamand <pabloenriquequintero@gmail.com>
2025-04-06 11:09:41 -0500

    v0.1

---
+++ dist/src/main/java/com/puj/cliente/clientePeticiones.java
@@ -0,0 +1,34 @@
+package com.puj.cliente;
+
+import java.util.Scanner;
+
+public class clientePeticiones {
+    public static void main(String[] args) {
+        String server = "192.168.10.6"; //Cambiar segun la IP del servidor
+        int port = 1080;
+        @SuppressWarnings("resource")
+        Scanner input = new Scanner(System.in);
+    
+        System.out.println("Conectando con el servidor");
+
+        System.out.println("\t||Peticion de salones y laboratorios||\n");
+
+        //Ingresar datos de peticion
+        System.out.println("Semestre: ");
+        int semestre = input.nextInt();
+
+        System.out.println("Facultad: ");
+        String facultad = input.nextLine();
+
+        System.out.println("Programa: ");
+        String programa = input.nextLine();
+
+        System.out.println("Salones requeridos: ");
+        int salones = input.nextInt();
+        
+        System.out.println("Laboratorios requeridos: ");
+        int laboratorios = input.nextInt();
+
+        System.out.println("Enviando peticion al servidor y esperado respuesta... ");
+    }
+}
