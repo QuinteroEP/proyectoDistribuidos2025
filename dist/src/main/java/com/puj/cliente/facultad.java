0cfc20f9cb1dc7d0798bf0c04197872763aaeede
Pablo Enrique Quintero Callamand <140966677+QuinteroEP@users.noreply.github.com>
2025-04-09 16:25:00 -0500

    v0.3
    parametros de entrada para facultad

--- dist/src/main/java/com/puj/cliente/facultad.java
+++ dist/src/main/java/com/puj/cliente/facultad.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class facultad {
     public static void main(String[] args) {
+        if(args.length != 4) {
+            System.out.println("\nError: uso incorrecto. Se requieren los parametros <nombre del programa> <semestre> <numero de salones requeridos> <numero de laboratorios requeridos>\n");
+            System.exit(1);
+        }
+        final String nombre = args[0];
+        final String semestre = args[1];
+        final String numeroSalones = args[2];
+        final String numeroLaboratorios = args[3];
+
+        //enviar datos de peticion
+        System.out.println("\nNueva peticion para el programa de " + nombre +" (semestre " + semestre + ")");
+        System.out.println("Salones requeridos: " + numeroSalones);
+        System.out.println("Laboratorios requeridos: " + numeroLaboratorios);
+        System.out.println("\n");
+
         String server = "192.168.10.6"; //Cambiar segun la IP del servidor
         int port = 1080;
         @SuppressWarnings("resource")
@@ -11,26 +26,6 @@ public class facultad {
     
         System.out.println("Conectando con el servidor");
 
-        System.out.println("\t||Peticion de salones y laboratorios||\n");
-
-        /*
-        //Ingresar datos de peticion
-        System.out.println("Semestre: ");
-        int semestre = input.nextInt();
-
-        System.out.println("Facultad: ");
-        String facultad = input.nextLine();
-
-        System.out.println("Programa: ");
-        String programa = input.nextLine();
-
-        System.out.println("Salones requeridos: ");
-        int salones = input.nextInt();
-        
-        System.out.println("Laboratorios requeridos: ");
-        int laboratorios = input.nextInt();
-
         System.out.println("Enviando peticion al servidor y esperado respuesta... ");
-         */
     }
 }
